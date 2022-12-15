54db79f7581351e89a9165de601bc44edd8f23ec
harit7 <harit.vishwakarma@gmail.com>
2022-12-15 05:13:09 -0600

    moon and circles

--- datasets/concentric_circles.py
+++ datasets/concentric_circles.py
@@ -4,9 +4,9 @@ import numpy as np
 from datasets.dataset_utils import *
 from utils.sampling_utils import *
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import make_moons
+from sklearn.datasets import make_circles
 
-class SyntheticMoon(DatasetNumpy):
+class ConcentricCircles(DatasetNumpy):
     def __init__(self,data_conf):
         self.data_conf = data_conf
         data_conf.setdefault('noise',0.05)
@@ -28,8 +28,8 @@ class SyntheticMoon(DatasetNumpy):
         n_samples = n_train + n_test 
         noise = data_conf['noise']
 
-        X,Y = make_moons(n_samples,noise=noise)
-        
+        X,Y = make_circles(n_samples=n_samples, factor=0.5, noise=noise)        
+
         X_train, X_test, Y_train, Y_test = train_test_split( X, Y, test_size=data_conf['test_set_size']
                                                             ,random_state=data_conf['random_state'] )
 
